0ebb26758c26b27eb6cf1c8fcc6139605182aeec
Patryk <patryk.janikowski@gmail.com>
2023-05-07 19:28:25 +0200

    init app

---
+++ src/app/pages/signin/signin.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SigninComponent } from './signin.component';
+import { RouterModule, Routes } from '@angular/router';
+import {MatInputModule} from '@angular/material/input';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: SigninComponent
+  }
+]
+
+@NgModule({
+  declarations: [
+    SigninComponent
+  ],
+  imports: [
+    CommonModule,
+    RouterModule.forChild(routes),
+    MatInputModule
+  ]
+})
+export class SigninModule { }
